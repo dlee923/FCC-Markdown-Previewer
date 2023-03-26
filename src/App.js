3ab717ad131333159d40468ec935056d99017aeb
Daniel Lee <dlee923@gmail.com>
2023-03-26 00:26:25 -0400

    updated function to hide previewer window if editor window is expanded and vice-versa.  Styled html from marked parser

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons';
-import { faExpandArrowsAlt } from '@fortawesome/free-solid-svg-icons';
+import { faExpandAlt, faExpandArrowsAlt } from '@fortawesome/free-solid-svg-icons';
 import { marked } from 'marked';
 
 let editorText = "# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here's some other cool stuff:\n\nHeres some code, `<div></div>`, between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n\tif (firstLine == '```' && lastLine == '```') {\n\t\treturn multiLineCode;\n\t}\n}\n```\n\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n\xa0- Some are bulleted.\n\xa0\xa0- With different indentation levels.\n\xa0\xa0\xa0- That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let's not forget embedded images:\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\n"
 
-const ExpandableContainer = ({title, content, expandFx}) => {
+const ExpandableContainer = ({title, content, expandFx, expandIcon}) => {
   return (
     <div className="expandable-container">
       <div className="expandable-container-header">
@@ -15,7 +15,7 @@ const ExpandableContainer = ({title, content, expandFx}) => {
           <FontAwesomeIcon icon={faFreeCodeCamp} />
           <p style={{ paddingLeft: "5px" }}>{title}</p>
         </div>      
-        <button onClick={expandFx}><FontAwesomeIcon icon={faExpandArrowsAlt}/></button>
+        <button onClick={expandFx}><FontAwesomeIcon icon={expandIcon}/></button>
       </div>
       {content}
     </div>
@@ -29,18 +29,18 @@ const EditorInput = ({onChangeFx, value}) => {
   );
 }
 
-const Editor = ({onChangeFx, value, expandFx}) => {
+const Editor = ({onChangeFx, value, expandFx, expandIcon}) => {
   return (
     <div className="editor">
-      <ExpandableContainer title="Editor" content={<EditorInput onChangeFx={onChangeFx} value={value}/>} expandFx={expandFx} />
+      <ExpandableContainer title="Editor" content={<EditorInput onChangeFx={onChangeFx} value={value}/>} expandFx={expandFx} expandIcon={expandIcon} />
     </div>    
   );
 }
 
-const Previewer = ({content, expandFx}) => {
+const Previewer = ({content, expandFx, expandIcon}) => {
   return (
-    <div className="previewer">
-      <ExpandableContainer title="Previewer" content=<div className="previewer-output" id="preview">{content}</div> expandFx={expandFx} />
+    <div className="previewer"> 
+      <ExpandableContainer title="Previewer" content=<div className="previewer-output" id="preview" dangerouslySetInnerHTML={{ __html: content}}></div> expandFx={expandFx} expandIcon={expandIcon} />
     </div>    
   );
 }
@@ -66,9 +66,15 @@ class App extends React.Component {
     if (this.state.isEditorExpanded) {
       // change height to small
       document.documentElement.style.setProperty("--editor-height", "35vh");
+      document.documentElement.style.setProperty("--previewer-max-height", "infinite");
+      document.documentElement.style.setProperty("--previewer-height", "30vh");
+      document.documentElement.style.setProperty("--previewer-margin-bottom", "20px");
     } else {
       // change height to large
       document.documentElement.style.setProperty("--editor-height", "100vh");
+      document.documentElement.style.setProperty("--previewer-max-height", "0");
+      document.documentElement.style.setProperty("--previewer-height", "0");
+      document.documentElement.style.setProperty("--previewer-margin-bottom", "0");
     }
     this.setState({
       isEditorExpanded: !this.state.isEditorExpanded
@@ -79,13 +85,17 @@ class App extends React.Component {
     console.log(this.state.isPreviewerExpanded);
     if (this.state.isPreviewerExpanded) {
       // change height to small
-      document.documentElement.style.setProperty("--previewer-height", "35vh");
+      document.documentElement.style.setProperty("--previewer-height", "30vh");
+      document.documentElement.style.setProperty("--editor-max-height", "infinite");
+      document.documentElement.style.setProperty("--editor-margin-top", "20px");
     } else {
       // change height to large
       document.documentElement.style.setProperty("--previewer-height", "100vh");
+      document.documentElement.style.setProperty("--editor-max-height", "0");
+      document.documentElement.style.setProperty("--editor-margin-top", "0");
     }
     this.setState({
-      isEditorExpanded: !this.state.isEditorExpanded
+      isPreviewerExpanded: !this.state.isPreviewerExpanded
     });
   }
 
@@ -96,10 +106,14 @@ class App extends React.Component {
   }
 
   render() {
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      breaks: true,
+    });
     return (
       <div className="App">
-        <Editor onChangeFx={this.handleTextChange} value={this.state.previewerText} expandFx={this.expandFxEditor} />
-        <Previewer content={marked.parse(this.state.previewerText)} expandFx={this.expandFx} />
+        <Editor onChangeFx={this.handleTextChange} value={this.state.previewerText} expandFx={this.expandFxEditor} expandIcon={this.state.isEditorExpanded ? faExpandAlt : faExpandArrowsAlt} />
+        <Previewer content={marked.parse(this.state.previewerText)} expandFx={this.expandFxPreviewer} expandIcon={this.state.isPreviewerExpanded ? faExpandAlt : faExpandArrowsAlt} />
       </div>
     );
   }
